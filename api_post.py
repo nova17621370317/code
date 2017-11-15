dd60f9d5100ce1570f45945afbb0cf0d9d80ed6d
nova <2153604401@qq.com>
2017-11-15 14:27:47 +0800

    api interface post

---
+++ api_post.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+#  params  headers  data   json   files
+#  post
+
+
+# import requests
+# import json
+#
+#
+# host = 'http://httpbin.org/'
+# endpoint = 'post'
+# url = ''.join([host,endpoint])
+#
+# params = {'show_env' : 1}
+# data = {'a':'评驾',"b":'form-data'}
+#
+# r = requests.post(url, params=params, data=data)
+#
+# print r.headers
+# print r.text
+
+list = [34798210, 13917645678, 2, 'weekly',1]
+print list
+
+list.sort()
+print list
+
+l = ['sysId','tel','scoretype','period','no']
+print l
+l.sort()
+print l
